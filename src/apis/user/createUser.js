d1bbb543fa106baf3089bfa9e8306e0258c8bd96
Văn Mạnh Bùi <buimanh17112001@gmail.com>
2021-08-04 22:12:51 +0700

    fix

--- src/apis/user/createUser.js
+++ src/apis/user/createUser.js
@@ -6,17 +6,17 @@ module.exports = createUser = (req, res, next) => {
   const userId = req.body._id
   console.log('current user: ', userId)
   try {
-    var users = db.getData('/users')
+    // var users = db.getData('/users')
 
-    if (users?.length > 0) {
-      let check = users.findIndex(user => user === userId)
-      if (check == -1) {
-        db.push('/users[]', userId, true)
-      }
-    } else {
-      db.push('/users[]', userId, true)
-    }
-
+    // if (users?.length > 0) {
+    //   let check = users.findIndex(user => user === userId)
+    //   if (check == -1) {
+    //     db.push('/users[]', userId, true)
+    //   }
+    // } else {
+    //   db.push('/users[]', userId, true)
+    // }
+    db.push('/users[]', userId, true)
 
     res.json({
       status: true,
--- src/routes/notification.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const registration = require('../apis/notification/registration')
-const trigger = require('../apis/notification/trigger')
-const router = express.Router()
-
-router.get('/trigger', trigger)
-router.post('/registrations', registration)
-
-module.exports = router
